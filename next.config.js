c0794059a1d878e85dda48b08ff1e1b32d707ca1
Esteban Contreras <contacto@econtreras.net>
2024-03-11 11:00:30 -0300

    chore: removing assetPrefix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: "/pomodoro",
   basePath: "/pomodoro",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
