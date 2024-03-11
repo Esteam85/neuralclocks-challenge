56fb70cf61f4de64923dbfe310a291f930fa8b68
Esteban Contreras <contacto@econtreras.net>
2024-03-10 22:43:17 -0300

    chore: adding assetPrefix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: "/pomodoro",
   basePath: "/pomodoro",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
